0fca3d07ab7feabbb34a3750d0fa3d2bef7dd51d
sbn <sbn@.com>
2018-11-03 13:43:22 +0800

    用户列表完成

--- src/views/user.vue
+++ src/views/user.vue
@@ -6,10 +6,10 @@
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
     <div class="searchArea">
-      <el-input placeholder="请输入内容" class="input-with-select searchInput">
-        <el-button slot="append" icon="el-icon-search"></el-button>
+      <el-input placeholder="请输入内容" v-model="usernamelist" class="input-with-select searchInput">
+        <el-button @click="selectuserlist" slot="append" icon="el-icon-search"></el-button>
       </el-input>
-      <el-button type="success">成功按钮</el-button>
+      <el-button type="success" @click="adduser">添加用户</el-button>
     </div>
     <el-table :data="tableData" style="width: 100%">
       <el-table-column type="index" label="#" width="40"></el-table-column>
@@ -24,6 +24,7 @@
       <el-table-column label="用户状态" width="80">
         <template slot-scope="scope">
           <el-switch
+            @change ="changeType(scope.row.id,scope.row.mg_state)"
             v-model="scope.row.mg_state"
             active-color="#13ce66"
             inactive-color="#ff4949">
@@ -32,9 +33,15 @@
       </el-table-column>
       <el-table-column label="操作" width="180">
         <template slot-scope="scope">
-          <el-button type="primary" icon="el-icon-edit" :plain="true" :circle="true" size="mini"></el-button>
-          <el-button type="success" icon="el-icon-check" :plain="true" :circle="true" size="mini"></el-button>
-          <el-button type="danger" icon="el-icon-delete" :plain="true" :circle="true" size="mini"></el-button>
+          <el-button type="primary" @click="selectUser(scope.row.id)" icon="el-icon-edit" :plain="true" :circle="true" size="mini">
+
+          </el-button>
+          <el-button type="success" @click="jusefenp(scope.row)" icon="el-icon-check" :plain="true" :circle="true" size="mini">
+
+          </el-button>
+          <el-button type="danger" @click="userDelete(scope.row.id)" icon="el-icon-delete" :plain="true" :circle="true" size="mini">
+
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -51,6 +58,66 @@
         </el-pagination>
       </div>
     </div>
+    <!-- 添加用户 -->
+      <el-dialog title="添加用户" :visible.sync="dialogFormVisibleadduser">
+        <el-form :model="formdata">
+          <el-form-item label="用户名" :label-width="formLabelWidth">
+            <el-input v-model="formdata.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" :label-width="formLabelWidth">
+            <el-input v-model="formdata.password"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" :label-width="formLabelWidth">
+            <el-input v-model="formdata.email"></el-input>
+          </el-form-item>
+          <el-form-item label="电话" :label-width="formLabelWidth">
+            <el-input v-model="formdata.mobile"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addusergo">确 定</el-button>
+        </div>
+      </el-dialog>
+    <!-- 编辑对话框 -->
+      <el-dialog title="编辑用户" :visible.sync="dialogFormVisibleEdituser">
+        <el-form :model="formdata">
+          <el-form-item label="用户名" :label-width="formLabelWidth">
+            <el-input disabled v-model="formdata.username"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" :label-width="formLabelWidth">
+            <el-input v-model="formdata.email"></el-input>
+          </el-form-item>
+          <el-form-item label="电话" :label-width="formLabelWidth">
+            <el-input v-model="formdata.mobile"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="editUser(formdata.id)">确 定</el-button>
+        </div>
+      </el-dialog>
+    <!-- 角色分配 -->
+      <el-dialog title="分配权限" :visible.sync="dialogTableVisibleJuse">
+        <el-form :model="formdata">
+            <el-form-item label="用户名" :label-width="formLabelWidth">
+              {{listuser}}
+            </el-form-item>
+          <el-form-item label="角色" :label-width="formLabelWidth">
+            <el-select v-model="routse">
+              <el-option :value="-1"  label="请选择">
+
+              </el-option>
+              <el-option v-for="(item,index) in options" :key="index" :label="item.roleName" :value="item.id">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="jusetijiao()">确 定</el-button>
+        </div>
+      </el-dialog>
   </el-card>
 </template>
 
@@ -59,14 +126,100 @@
     data () {
       return {
         tableData: [],
+        usernamelist: '',
+        usersType:'',
         pagenum:1,
         pagesize:2,
         currentPage: 1,
-        total:0
+        total:0,
+        dialogFormVisibleEdituser: false,
+        dialogTableVisibleJuse: false,
+        dialogFormVisibleadduser: false,
+        formLabelWidth: '100px',
+        formdata: {
+          username: '',
+          password: '',
+          email: '',
+          mobile: ''
+        },
+        listuser: '',
+        options: [],
+        routse: -1,
+        userId: ''
       }
     },
 
     methods: {
+      selectuserlist(){
+        this.getList()
+      },
+      async addusergo(){
+        const res = await this.$http.post(`users`,this.formdata)
+        this.$message.success(res.data.meta.msg)
+        this.dialogFormVisibleadduser = false
+        this.getList()
+      },
+      adduser(){
+        this.dialogFormVisibleadduser = true
+      },
+      userDelete(id) {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+          center: true
+        }).then(async () => {
+          const res = await this.$http.delete(`users/${id}`)
+          if(res){this.$message({ type: 'success', message: '删除成功!'});}
+          this.getList()
+        }).catch(() => {
+          this.$message({ type: 'info', message: '已取消删除' });
+        });
+      },
+      async jusetijiao(id){
+        const res = await this.$http.put(`users/${this.userId}/role`,{rid:this.routse})
+        this.dialogTableVisibleJuse = false
+        const { meta: { status, msg }} = res.data
+        this.$message.success(msg)
+        this.getList()
+      },
+      async huoquuserjiaose(id){
+        const res = await this.$http.get(`users/${id}`)
+        this.routse = res.data.data.rid
+      },
+      async jusefenp(user){
+        this.dialogTableVisibleJuse = true
+        const res = await this.$http.get(`roles`)
+        this.huoquuserjiaose(user.id)
+        this.userId = user.id;
+        this.listuser = user.username
+        this.options = res.data.data
+      },
+      async editUser(id){
+        this.dialogFormVisibleEdituser = false
+        const res = await this.$http.put(`users/${id}`,this.formdata)
+        if(res.data.meta.status === 200){
+          this.$message.success(res.data.meta.msg)
+          this.getList()
+        }
+      },
+      async selectUser (id) {
+        this.dialogFormVisibleEdituser = true
+        const res = await this.$http.get(`users/${id}`)
+        const {data, meta} = res.data;
+        if(meta.status === 200 ){
+          this.formdata = data
+        }
+      },
+      async changeType (id,type) {
+        const res = await this.$http.put(`users/${id}/state/${type}`)
+        const {msg,status} = res.data.meta
+        if (status === 200){
+          this.$message.success(msg);
+        } else {
+          this.$message.warning(msg)
+        }
+      },
       handleSizeChange (pagesize) {
         this.currentPage = 1
         this.pagesize = pagesize
@@ -76,14 +229,14 @@
         this.pagenum = pagenum
         this.getList()
       },
+      // 查询用户
       async getList() {
         const AUTH_TOKEN = sessionStorage.getItem('token')
         this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
-        const list = await this.$http.get(`users?pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
+        const list = await this.$http.get(`users?pagenum=${this.pagenum}&pagesize=${this.pagesize}&query=${this.usernamelist}`)
         if(list.status === 200){
           this.tableData = list.data.data.users
           this.total = list.data.data.total
-          // console.log(list.data.data.total)
         }
       },
 
